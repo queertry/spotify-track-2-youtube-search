7f6a019d8c9d087ac25b40b6958ab4245d63de5a
Queertry <queertry.blog@gmail.com>
2026-03-02 14:39:08 +0100

    Actually, lets include the license directly into each release

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ fi
 
 mkdir -p "build/$version"
 
-zip -r "build/$version/st2ys@queertry.com.xpi" src/ manifest.json
+zip -r "build/$version/st2ys@queertry.com.xpi" src/ manifest.json LICENSE
 echo "Successfully built version $version"
 exit 0
